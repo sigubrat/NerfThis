c4632d107491ce36b2c34caf33fa3f9d5dc8f98c
Sigubrat <Sigurdjbratt@hotmail.no>
2023-08-29 11:03:52 +0200

    Connect webcam

--- src/main/java/NerfThis.java
+++ src/main/java/NerfThis.java
@@ -1,6 +1,15 @@
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.core.MatOfRect;
@@ -11,44 +20,68 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.objdetect.Objdetect;
+import org.opencv.videoio.VideoCapture;
 
 import nu.pattern.OpenCV;
 
-public class NerfThis {
+public class NerfThis extends Application {
 	public static void main(String[] args) {
-		detectFaceFromWebcam();
+		OpenCV.loadLocally();
+		launch(args);
 	}
 
-	public static void detectFaceFromWebcam() {
-		System.out.println("Test");
+	@Override
+	public void start(Stage primaryStage) {
+		detectFaceFromWebcam(primaryStage);
+	}
+
+	public static void detectFaceFromWebcam(Stage stage) {
+		VideoCapture capture = new VideoCapture(0);
+		ImageView imageView = new ImageView();
+		HBox hbox = new HBox(imageView);
+		Scene scene = new Scene(hbox);
+		Platform.runLater(() -> {
+			stage.setScene(scene);
+			stage.show();
+		});
+
+
+		new AnimationTimer() {
+			@Override
+			public void handle(long l) {
+				imageView.setImage(getCapture(capture));
+			}
+		}.start();
 	}
 
 	public static void detectFaceImage() {
-		OpenCV.loadLocally();
-
 		String sourceImagePath = "src/main/resources/face.jpeg";
 		String targetImagePath = "src/main/resources/result.jpeg";
 
-
 		Mat loadedImage = loadImage(sourceImagePath);
-		int minFaceSize = Math.round(loadedImage.rows() * 0.1f); 
+		int minFaceSize = Math.round(loadedImage.rows() * 0.1f);
 		MatOfRect facesDetected = new MatOfRect();
 		CascadeClassifier cascadeClassifier = new CascadeClassifier();
 		cascadeClassifier.load("./src/main/resources/haarcascades/haarcascade_frontalface_alt.xml");
-		cascadeClassifier.detectMultiScale(loadedImage, 
-				facesDetected, 
-				1.1, 
-				3, 
-				Objdetect.CASCADE_SCALE_IMAGE, 
-				new Size(minFaceSize, minFaceSize), 
-				new Size() 
-				);
+		cascadeClassifier.detectMultiScale(loadedImage,
+				facesDetected,
+				1.1,
+				3,
+				Objdetect.CASCADE_SCALE_IMAGE,
+				new Size(minFaceSize, minFaceSize),
+				new Size());
 
-		Rect[] facesArray = facesDetected.toArray(); 
-		for(Rect face : facesArray) { 
-			Imgproc.rectangle(loadedImage, face.tl(), face.br(), new Scalar(0, 0, 255), 3); 
-		} 
+		Rect[] facesArray = facesDetected.toArray();
+		for (Rect face : facesArray) {
+			Imgproc.rectangle(loadedImage, face.tl(), face.br(), new Scalar(0, 0, 255), 3);
+		}
 		saveImage(loadedImage, targetImagePath);
+	}
+
+	public static Image getCapture(VideoCapture capture) {
+		Mat mat = new Mat();
+		capture.read(mat);
+		return matToImg(mat);
 	}
 
 	public static Mat loadImage(String imagePath) {
@@ -57,11 +90,11 @@ public class NerfThis {
 	}
 
 	public static void saveImage(Mat imageMatrix, String targetPath) {
-    	Imgcodecs imgcodecs = new Imgcodecs();
-    	imgcodecs.imwrite(targetPath, imageMatrix);
+		Imgcodecs imgcodecs = new Imgcodecs();
+		imgcodecs.imwrite(targetPath, imageMatrix);
 	}
 
-	public Image matToImg(Mat mat) {
+	public static Image matToImg(Mat mat) {
 		MatOfByte bytes = new MatOfByte();
 		Imgcodecs.imencode("img", mat, bytes);
 		InputStream InputStream = new ByteArrayInputStream(bytes.toArray());
